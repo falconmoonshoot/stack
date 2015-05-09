1f4c88f9f04592cffb2bbeb5cff9070aaac67538
Sandy Barnabas <sbarnabas@gmail.com>
2015-05-09 14:24:05 -0400

    added player to landing page

--- client/components/landingpage/landingpage.controller.js
+++ client/components/landingpage/landingpage.controller.js
@@ -4,11 +4,17 @@ angular.module('moonshootApp')
   .controller('LandingPageCtrl', function ($scope, $location, $stateParams, $firebaseObject) {
      $scope.effortGuidPath =  $stateParams.effortGuid;
     var ref = new Firebase("https://glowing-torch-9335.firebaseio.com/"+$scope.effortGuidPath);
-    console.log ($scope.effortGuidPath);
     var syncObject = $firebaseObject(ref);
     syncObject.$bindTo($scope, "effortGuid");
+    var unwatch = syncObject.$watch(function() {
 
-    $scope.isCollapsed = true;
+      if(syncObject.videoGuid) {
+       ZiggeoApi.Embed.embed("#player", {video: syncObject.videoGuid, modes:['player'], responsive:true});
+       console.log("loaded"); 
+     }
+    });
+
+   $scope.isCollapsed = true;
 
     $scope.isActive = function(route) {
       return route === $location.path();
