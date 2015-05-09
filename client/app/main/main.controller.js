69a4f3cb47dfa1ed4b0f8dc7d2e33af350a48392
kr4ng <steven.m.simoni@gmail.com>
2015-05-09 13:28:37 -0400

    my commits

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,10 +1,25 @@
 'use strict';
 
 angular.module('moonshootApp')
-  .controller('MainCtrl', function ($scope, $http) {
+  .controller('MainCtrl', function ($scope, $http, $filter, $firebaseObject) {
+    $scope.userId = 'dsaf'
     $scope.contacts = [{ name: 'Patrick Malley', type: 'Contact'},
                        { name: 'Patrick Malley', type: 'Contact'},
                        { name: 'Patrick Malley', type: 'Contact'},
                       ];
+
+    $scope.selectedContacts = [];
+    $scope.selectedContacts = function () {
+      $scope.falconcontacts = $filter('filter')($scope.selectedContacts, {checked: true});
+    }
+
+    ZiggeoApi.Events.on("submitted", function (data) {
+        // Triggered when a video has been recorded 
+        $scope.recordingUniqueId = data.video.token;
+        console.log('unique id ', $scope.recordingUniqueId);
+        $scope.$apply(function () {
+              $scope.videoRecorded = true;
+        });
+    });
 
   });
