ef593a9e11e8fe1f99fa0033c929c422ac781807
kr4ng <steven.m.simoni@gmail.com>
2015-05-09 12:26:35 -0400

    d

--- client/app/app.js
+++ client/app/app.js
@@ -11,5 +11,21 @@ angular.module('moonshootApp', [
     $urlRouterProvider
       .otherwise('/');
 
+    $stateProvider 
+            // HOME STATES AND NESTED VIEWS ========================================
+            .state('home', {
+                url: '/home',
+                abstract:true,
+                templateUrl: 'components/home/home.html',
+            })
+            .state('analytics', {
+                url: '/analytics',
+                templateUrl: 'components/analytics/analytics.html',     
+            })
+            .state('landingpage', {
+                url: '/personal',
+                templateUrl: 'components/landingpage/landingpage.html',
+            })
+
     $locationProvider.html5Mode(true);
   });
