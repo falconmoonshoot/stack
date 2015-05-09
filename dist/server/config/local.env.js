bbd6db11b4d4045d4caebd34ddac1b71c0c3edc2
James Maidment <james@architizer.com>
2015-05-09 19:33:34 -0400

    Updates

--- dist/server/config/local.env.js
+++ dist/server/config/local.env.js
@@ -6,18 +6,8 @@
 // You will need to set these on the server you deploy to.
 
 module.exports = {
-<<<<<<< HEAD
-<<<<<<< HEAD
-  DOMAIN:           'http://dev.falconmoonshoot.com:9000',
-  SESSION_SECRET:   'moonshoot-secret',
-
-=======
-  DOMAIN: 'http://localhost:9000',
-=======
   DOMAIN: 'http://dev.falconmoonshoot.com:9000',
->>>>>>> Added dynamic events
   SESSION_SECRET: "moonshoot-secret",
->>>>>>> 51d27b99bd9e86d5e62c02041940d77164cfdbdb
   // Control debug level for modules using visionmedia/debug
   DEBUG: ''
 };
