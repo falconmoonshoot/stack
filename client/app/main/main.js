025f206bd93cc320465bcb48e1dcf5fa62a3cda2
James Maidment <james@architizer.com>
2015-05-09 11:36:39 -0400

    Initial commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('moonshootApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
